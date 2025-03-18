a947ca73789c6a2386feab0f1e09f77d8062b7d4
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-03-18 22:55:44 +0700

    update signin and login function

--- SocialMediaProject/app/src/main/java/com/example/socialmediaproject/LoginActivity.kt
+++ SocialMediaProject/app/src/main/java/com/example/socialmediaproject/LoginActivity.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var title: TextView
     private lateinit var signuptext: TextView
     private lateinit var intent: Intent
+    private lateinit var loginbutton: Button
+    private lateinit var firebaseauth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,27 @@ class LoginActivity : AppCompatActivity() {
         SetTitleGradientColor(title)
         signuptext=findViewById(R.id.signUpText)
         signuptext.setOnClickListener { OnSignUpClicked() }
+        loginbutton=findViewById(R.id.loginButton)
+        firebaseauth=FirebaseAuth.getInstance()
+        loginbutton.setOnClickListener {
+            val email=findViewById<TextInputEditText>(R.id.email)
+            val password=findViewById<TextInputEditText>(R.id.password)
+            if (email.text.toString().isNotEmpty() && password.text.toString().isNotEmpty())
+            {
+                firebaseauth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnCompleteListener(this) {
+                        task->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show()
+                                intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                            else Toast.makeText(this, "Đăng nhập thất bại!", Toast.LENGTH_SHORT).show()
+                    }
+            }
+            else Toast.makeText(this, "Vui lòng nhập đầy đủ các trường!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun SetTitleGradientColor(title: TextView)
--- SocialMediaProject/app/src/main/java/com/example/socialmediaproject/SignUpActivity.kt
+++ SocialMediaProject/app/src/main/java/com/example/socialmediaproject/SignUpActivity.kt
@@ -4,19 +4,24 @@ import android.content.Intent
 import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewbinding.ViewBinding
 import com.example.socialmediaproject.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var logintext: TextView
     private lateinit var intent: Intent
     private lateinit var title: TextView
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var signupbutton: Button
+    private lateinit var firebaseauth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +37,30 @@ class SignUpActivity : AppCompatActivity() {
         logintext.setOnClickListener { OnLoginTextClikec() }
         title=binding.title
         SetTitleGradientColor(title)
+        signupbutton=binding.signUpButton
+        firebaseauth=FirebaseAuth.getInstance()
+        signupbutton.setOnClickListener {
+            val email=binding.email.text.toString()
+            val password=binding.password.text.toString()
+            val confirmpassword=binding.confirmPassword.text.toString()
+            val name=binding.name.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty() && confirmpassword.isNotEmpty())
+                if (password==confirmpassword)
+                    firebaseauth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                        task -> if (task.isSuccessful)
+                        {
+                            Toast.makeText(this, "Đăng kí tài khoản thành công!", Toast.LENGTH_SHORT).show()
+                            intent=Intent(this,LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else
+                            Toast.makeText(this, "Đăng kí tài khoản thất bại!", Toast.LENGTH_SHORT).show()
+                    }
+                else Toast.makeText(this, "Mật khẩu nhập lại không đúng!", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "Vui lòng nhập đầy đủ các trường!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun OnLoginTextClikec()
