bdba6a55edaa1b622e93c0ba50e1b5ec39d1f883
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-04-06 20:32:48 +0700

    mainpage updates

--- SocialMediaProject/app/src/main/java/com/example/socialmediaproject/ui/mainpage/MainPageFragment.kt
+++ SocialMediaProject/app/src/main/java/com/example/socialmediaproject/ui/mainpage/MainPageFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.socialmediaproject.R
 import com.example.socialmediaproject.databinding.FragmentMainPageBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -27,21 +28,37 @@ class MainPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val wallUserId=arguments?.getString("wall_user_id")
-        val currentUserId=auth.currentUser?.uid ?: ""
-        if (currentUserId==wallUserId) {
-            binding.buttonAddFriend.visibility=View.GONE
-        }
-        else {
-            db.collection("Users").document(currentUserId).get().addOnSuccessListener {
-                result->if (result.exists()) {
-                    val friends=result.get("friends") as? List<String>
-                    if (friends?.contains(wallUserId) == true) {
-                        binding.buttonAddFriend.visibility = View.GONE
-                        binding.buttonUnfriend.visibility = View.VISIBLE
-                        binding.buttonChat.visibility = View.VISIBLE
+        val wallUserId=arguments?.getString("wall_user_id") ?: ""
+        binding.progressBar.visibility=View.VISIBLE
+        db.collection("Users").document(wallUserId).get().addOnSuccessListener {
+            result->if (result.exists()) {
+                Glide.with(requireContext()).load(result.getString("avatarurl"))
+                    .placeholder(R.drawable.avataricon)
+                    .error(R.drawable.avataricon)
+                    .into(binding.profileAvatar)
+                binding.profileUsername.text = result.getString("name")
+                binding.profileBio.text = result.getString("bio")
+                Glide.with(requireContext()).load(result.getString("wallurl"))
+                    .placeholder(R.drawable.profile_background_gradient)
+                    .error(R.drawable.profile_background_gradient)
+                    .into(binding.wallImage)
+                val currentUserId=auth.currentUser?.uid ?: ""
+                if (currentUserId==wallUserId) {
+                    binding.buttonAddFriend.visibility=View.GONE
+                }
+                else {
+                    db.collection("Users").document(currentUserId).get().addOnSuccessListener {
+                            result->if (result.exists()) {
+                            val friends=result.get("friends") as? List<String>
+                            if (friends?.contains(wallUserId) == true) {
+                                binding.buttonAddFriend.visibility = View.GONE
+                                binding.buttonUnfriend.visibility = View.VISIBLE
+                                binding.buttonChat.visibility = View.VISIBLE
+                            }
+                        }
                     }
                 }
+                binding.progressBar.visibility = View.GONE
             }
         }
     }
