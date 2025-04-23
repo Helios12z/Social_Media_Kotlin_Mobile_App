155aec4725a92bbd6470d2c9c301c8ea542ee3ae
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-04-24 02:04:35 +0700

    Update NotificationAdapter.kt

--- SocialMediaProject/app/src/main/java/com/example/socialmediaproject/adapter/NotificationAdapter.kt
+++ SocialMediaProject/app/src/main/java/com/example/socialmediaproject/adapter/NotificationAdapter.kt
@@ -6,13 +6,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.socialmediaproject.R
 import com.example.socialmediaproject.databinding.ItemNotificationBinding
 import com.example.socialmediaproject.dataclass.Notification
+import com.google.firebase.firestore.FirebaseFirestore
 
 class NotificationAdapter(private val onClick: (Notification) -> Unit, private val onDeleteClick: (Notification) -> Unit): ListAdapter<Notification, NotificationAdapter.NotificationViewHolder>(NotificationDiffCallback()) {
     inner class NotificationViewHolder(val binding: ItemNotificationBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(notification: Notification) {
+            FirebaseFirestore.getInstance().collection("Users").document(notification.senderId).get().addOnSuccessListener {result->
+                Glide.with(binding.root.context)
+                    .load(result.getString("avatarurl"))
+                    .placeholder(R.drawable.avataricon)
+                    .error(R.drawable.avataricon)
+                    .into(binding.notificationIcon)
+            }
+            .addOnFailureListener {
+                Glide.with(binding.root.context)
+                    .load(R.drawable.avataricon)
+                    .into(binding.notificationIcon)
+            }
             binding.notificationText.text = notification.message
             binding.notificationTime.text = DateUtils.getRelativeTimeSpanString(notification.timestamp.toDate().time)
             binding.notificationCard.strokeWidth = if (!notification.read) 4 else 0
